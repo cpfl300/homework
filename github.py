0960f66d8d3a63002089a374148742e402bc3c3f
Minju <cpfl300@naver.com>
2013-03-18 22:14:40 +0900

    homework END

---
+++ github.py
@@ -0,0 +1,13 @@
+https://github.com/cpfl300/homework.gitmake directory : mkdir
+remove          : rmdir
+
+use git!!
+
+1. mkdir 'name'
+2. cd 'name'
+3. git init
+4. if you type 'ls -a' you can see the '.git' file
+
+
+** git clone "http://~~"
+git push -u origin master
